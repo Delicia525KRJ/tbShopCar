233fbf14426ceec23913ac891198cff0a3eb4717
zhangshuyu <1352167300@qq.com>
2021-07-26 10:33:14 +0800

    淘宝简单页面代码第一次提交

---
+++ src/view/Home/index.vue
@@ -0,0 +1,433 @@
+<template>
+    <div class="tb_home">
+        <!-- 首页搜索部分 -->
+        <div class="tb_search">
+            <img src="https://img.alicdn.com/tfs/TB1R5fsgyDsXe8jSZR0XXXK6FXa-281-80.jpg">
+            <div class="search_click">
+                <div class="click_inputP">
+                    <el-input class="inputP_input" v-model="input" placeholder="淘宝网！"></el-input>
+                    <p class="inputP_p">搜索</p>
+                </div>
+                <div class="click_p">
+                    <p v-for="item in tbSearchLlist" :key="item.id">{{item.p}}</p>
+                </div>
+            </div>
+        </div>
+        <!-- 首页内容图片部分 -->
+        <div class="tb_content">
+            <div class="content_div" v-for="item in tbContentList" :key="item.id" @mouseenter="changeBorderColor" @mouseleave="changeBorderColorN">
+                <div class="content_card" :style="isMouseChange ? {borderColor: '#d3d3d3', cursor:'pointer'} : {borderColor: '#d3d3d3'}" @click="enterStore" >
+                    <img :src="item.src">
+                    <p>{{item.p}}</p>
+                    <div class="card_span">
+                        <span>¥{{item.span1}}</span>
+                        <span>¥{{item.span2}}</span>
+                    </div>
+                    <div class="card_shop">
+                        <i class="el-icon-s-shop"></i>
+                        <span>{{item.shop}}</span>
+                    </div>
+                    <el-divider></el-divider>
+                    <p class="card_sale">{{item.sale}}</p>
+                </div>
+            </div>
+            <div class="content_page">
+                <el-pagination background layout="prev, pager, next" :total="10"></el-pagination>
+            </div>
+            <div class="content_other">
+                <span>您是不是想找：</span>
+                <div class="other_span" v-for="item in tbContentOtherList" :key="item.id">
+                    <span class="nextLine_for">{{item.span}}</span>
+                    <span>|</span>
+                </div>
+            </div>
+        </div>
+        <!-- 首页页脚部分 -->
+        <div class="tb_footer">
+            <div class="footer_firstLine">
+                <span  v-for="item in tbFooterFirstList" :key="item.id">{{item.span}}</span>
+                <img src="../../assets/images/zhenggongwang.png" alt="">
+                <span>浙公网安备 33010002000075号</span>
+            </div>
+            <div class="footer_nextLine">
+                <div v-for="item in tbFooterNextList" :key="item.id">
+                    <span class="nextLine_for">{{item.span}}</span>
+                    <span v-if="item.span !== '达摩院'">|</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Home',
+        data(){
+            return {
+                input: '',
+                isMouseChange: false,
+                tbSearchLlist: [
+                    {p: '一淘限时抢'},
+                    {p: '项链女'},
+                    {p: '休闲裤'},
+                    {p: '保温杯'},
+                    {p: '洗衣液'},
+                    {p: '卫衣'},
+                    {p: '女手表'},
+                    {p: '男士t恤'},
+                    {p: '窗帘'},
+                    {p: '帆布鞋'},
+                    {p: '女内衣'},
+                ],
+                tbContentList: [
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+
+                    {
+                        src: require('../../assets/images/content_card.webp'),
+                        p: '新手淘宝开店免费注册如何开网店教程宝贝代上架货源店铺运营推广',
+                        span1: '100.00',
+                        span2: '100.00',
+                        shop: '闲学旗舰店',
+                        sale: '月销 283',
+                    },
+                ],
+                tbContentOtherList: [
+                    {span: '斜跨女包'},
+                    {span: '女裤'},
+                    {span: '笔记本'},
+                    {span: '笔袋'},
+                    {span: 'mac'},
+                    {span: '耐克'},
+                    {span: '无线耳机'},
+                    {span: '男士t恤'},
+                    {span: '苹果'},
+                    {span: '男拖鞋'},
+                ],
+                tbFooterFirstList: [
+                    {span: '联系客服'},
+                    {span: '开放平台'},
+                    {span: '法律声明'},
+                    {span: '廉正举报'},
+                    {span: 'Taobao.com版权所有 2003-现在'},
+                    {span: '增值电信业务经营许可证：浙B2-20070195'},
+                ],
+                tbFooterNextList: [
+                    {span: '阿里巴巴集团', },
+                    {span: '淘宝网', },
+                    {span: '天猫', },
+                    {span: '聚划算', },
+                    {span: '全球速卖通', },
+                    {span: '阿里巴巴国际交易市场', },
+                    {span: '1688', },
+                    {span: '1688', },
+                    {span: '飞猪', },
+                    {span: '阿里云计算', },
+                    {span: 'AliOS', },
+                    {span: '阿里通信', },
+                    {span: '高德', },
+                    {span: 'UC', },
+                    {span: '友盟', },
+                    {span: '虾米', },
+                    {span: '阿里星球', },
+                    {span: '钉钉', },
+                    {span: '支付宝', },
+                    {span: '达摩院',}, 
+                ],
+            }
+        },
+        methods: {
+            changeBorderColor(){
+                this.isMouseChange = true;
+            },
+            changeBorderColorN(){
+                this.isMouseChange = false;
+            },
+            enterStore(){
+                this.$router.push('../../Store');
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.tb_home{
+    display: flex;
+    flex-direction: column;
+}
+// 首页搜索部分
+.tb_search{
+    width: 60%;
+    margin-left: 15%;
+    display: flex;
+    flex-direction: row;
+    margin-bottom: 100px;
+}
+.search_click{
+    width: 100%;
+    display: flex;
+    flex-direction: column; 
+}
+.click_inputP{
+    display: flex;
+    flex-direction: row;
+}
+.inputP_input{
+    // 修改element-ui的默认样式
+    /deep/ .el-input__inner{
+        border: 4px solid red;
+        border-radius: 0;
+    }
+}
+.inputP_p{
+    margin:0 auto;
+    width: 150px;
+    height: 40px;
+    text-align: center;
+    background-color: red;
+    color: #fff;
+    line-height: 40px;
+}
+.click_p{
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    p{
+        color: #d3d3d3;
+        font-size: 10px;
+    }
+    p:hover{ 
+        color:red;
+    }
+}
+// 首页内容图片部分
+.tb_content{
+    width: 64%;
+    height: 1300px;
+    margin-left: 15%;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    align-content: flex-start;
+    margin-bottom: 50px;
+}
+.content_card{
+    width: 194px;
+    height: 344px;
+    padding: 20px 22px;
+    border: 1px solid #d3d3d3;
+    img{
+        width: 100%;
+    }
+    p{
+        color: #d3d3d3;
+        font-size: 15px;
+        text-align: left; 
+        // 超过两行的文字，省略号显示
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+    }
+    .card_span{
+        text-align: left;
+        span:nth-child(2){
+            margin-left: 15px;
+            color: #d3d3d3;
+            font-size: 15px;
+            // 删除线
+            text-decoration:line-through;
+        }
+    }
+    .card_shop{
+        text-align: left;
+        margin-top: 10px;
+        .el-icon-s-shop{
+            margin-right: 10px;
+            color: red;
+        }
+        span{
+            color: #d3d3d3;
+            font-size: 15px;
+        }
+    }
+    /deep/ .el-divider--horizontal{
+        margin: 5px 0;
+    }
+    .card_sale{
+        color: #d3d3d3;
+        font-size: 15px;
+        text-align: right;
+    }
+}
+#mourse_change{
+    border: 1px solid red;
+}
+.content_page{
+    width: 100%;
+    height: 20px;
+    margin-top: 20px;
+}
+.content_other{
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    margin-top: 50px;
+    background-color: #d3d3d3;
+    display: flex;
+    flex-direction: row;
+    color: black;
+    span{
+        margin-left: 5px;
+    }
+    .other_span{
+        display: inline;
+        margin-right: 10px;
+        span:nth-child(1){
+            margin-right: 10px;
+        }
+    }
+}
+// 首页页脚部分
+.tb_footer{
+    width: 64%;
+    margin-left: 15%;
+    height: 100px;
+    span{
+        color: #d3d3d3;
+        font-size: 10px;
+        text-align: left;
+        margin-right: 20px;
+    }
+    .footer_firstLine{
+        display: flex;
+        flex-direction: row;
+        span:hover{ 
+            color: black;
+        }
+    }
+    .footer_nextLine{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        span{
+            font-size: 15px;
+        }
+        span:hover{ 
+            color: black;
+        }
+    }
+}
+</style>
---
+++ src/view/Store/index.vue
@@ -0,0 +1,167 @@
+<template>
+    <div class="store">
+        <!-- 店铺名称 -->
+        <div class="store_name">
+            <img src="//gdp.alicdn.com/imgextra/i2/3070955695/TB29pWms5CYBuNkHFCcXXcHtVXa_!!3070955695.jpg" alt="">
+        </div>
+        <!-- 店铺内容 -->
+        <div class="store_content">
+            <div class="content_leftImg">
+                <el-carousel>
+                    <el-carousel-item v-for="item in contentLeftImgList" :key="item.id">
+                        <img :src="item.src" alt="">
+                    </el-carousel-item>
+                </el-carousel>
+            </div>
+            <div class="content_rightInfo">
+                <el-form ref="contentRightInfoData" :model="contentRightInfoData" label-width="80px">
+                    <p>项链女纯银梦幻星球玫瑰金吊坠潮小众轻奢锁骨链简约气质生日礼物</p>
+                    <div class="rightInfo_span">
+                        <span>免费刻字</span>
+                        <span>代写贺卡</span>
+                        <span>精美包装</span>
+                    </div>
+                    <div class="rightInfo_price">
+                        <span>价格</span>
+                        <span>¥</span>
+                        <span>{{contentRightInfoData.price}}</span>
+                    </div>
+                    <div class="rightInfo_amount">
+                        <span>数量</span>
+                        <el-input-number v-model="num" :step="1" :min="0" @change="chagneShopAmount"></el-input-number>
+                    </div>
+                    <div class="rightInfo_totalAmount">
+                        <span>合计</span>
+                        <el-input size="small" v-model="contentRightInfoData.totalAmount"></el-input>
+                        <span>元</span>
+                    </div>
+                    <div class="rightInfo_shop">
+                        <el-button type="danger" plain>立即购买</el-button>
+                        <el-button type="danger" icon="el-icon-shopping-cart-2">加入购物车</el-button>
+                    </div>
+                </el-form>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'store',
+        data(){
+            return {
+                num: 0,
+                contentRightInfoData: {
+                    price: 200,
+                    totalAmount: 0,
+                },
+                contentLeftImgList: [
+                    {src: require("../../assets/images/store_img1.jpg")},
+                    {src: require("../../assets/images/store_img2.jpg")},
+                    {src: require("../../assets/images/store_img1.jpg")},
+                    {src: require("../../assets/images/store_img2.jpg")},
+                ]
+            }
+        },
+        methods: {
+            chagneShopAmount(num){
+                this.contentRightInfoData.totalAmount = this.contentRightInfoData.price * num;
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.store{
+    width: 80%;
+    margin-left: 10%;
+}
+// 店铺内容
+.store_content{
+    width: 100%;
+    margin-left: 15%;
+    margin-top:50px;
+    display: flex;
+    flex-direction: row;
+    // 店铺内容左边图片
+    .content_leftImg{
+        width: 280px;
+        height: 310px;
+        .el-carousel{
+            img{
+                height: 100%;
+                width: 100%;
+            }
+        }
+    }
+    // 店铺内容右边信息
+    .content_rightInfo{
+        display: flex;
+        flex-direction: column;
+        margin-left: 100px;
+        p{
+            font-size: 20px;
+            font-weight: bold;
+            margin: 0;
+        }
+        span{
+            font-size: 15px;
+            margin: 0;
+            text-align: left;
+        }
+        .rightInfo_span{
+            font-size: 15px;
+            color: red;
+            margin: 0;
+            text-align: left;
+        }
+        .rightInfo_price{
+            width: 100%;
+            height: 50px;
+            margin: 10px 0;
+            background-color: #e9e9e9;
+            display: flex;
+            flex-direction: row;
+            span:nth-child(1){
+                line-height: 50px;
+                padding-left: 10px;
+                padding-right: 150px;
+            }
+            span:nth-child(2), span:nth-child(3){
+                font-size: 25px;
+                color: red;
+                font-weight: bold;
+                line-height: 50px;
+            }
+        }
+        .rightInfo_amount{
+            display: flex;
+            flex-direction: row;
+            span{
+                margin-right: 50px;
+            }
+        }
+        .rightInfo_totalAmount{
+            margin: 20px 0;
+            display: flex;
+            flex-direction: row;
+            span:nth-child(1){
+                padding-right: 50px;
+            }
+            .el-input{
+                width: 180px;
+            }
+        }
+        .rightInfo_shop{
+            display: flex;
+            flex-direction: row;
+            justify-content: flex-start;
+            margin: 20px 80px;
+            .el-button{
+                width: 200px;
+                height: 40px;
+            }
+        }
+    }
+}
+</style>
